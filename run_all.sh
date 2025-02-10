41bccf266c899145412a51dfb62a6e40fb5d818f
Ali Aghaeifar <ali.aghaeifar@tuebingen.mpg.de>
2025-02-10 09:12:30 +0100

    clean

--- run_all.sh
+++ run_all.sh
@@ -6,35 +6,13 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 # Change to the script directory
 pushd "$SCRIPT_DIR" > /dev/null
 
+## Find and delete all directories named "config"
+find . -type d -name "configs" -exec rm -rf {} +
+find . -type d -name "outputs" -exec rm -rf {} +
+find . -type f -name "log_*" -exec rm -rf {} +
+# find . -type d -name "phantoms" -exec rm -rf {} +
 
-#!/bin/bash
-
-# List of scripts to run
-scripts=(
-    "./benchmark/generate_cylinders.sh"
-    "./grase_cpmg_ste/generate_cylinders.sh"
-    "./BVF/generate_cylinders.sh"
-    "./orientation/generate_cylinders.sh"
-    "./virtual_fov/generate_cylinders.sh"
-    "./permeability/generate_cylinders.sh"
-    "./benchmark/run.sh"    
-    "./BVF/run.sh"    
-    "./grase_cpmg_ste/run.sh"    
-    "./orientation/run.sh"    
-    "./virtual_fov/run.sh"    
-    "./permeability/run.sh"
-)
-
-# Loop through each script and execute it
-for script in "${scripts[@]}"
-do
-    if [ -x "$script" ]; then
-        echo "Running $script..."
-        "$script"
-    else
-        echo "Skipping $script as it is not executable"
-    fi
-done
-
-
-
+## Find and execute all scripts named "xyz.sh"
+# find . -type f -name "generate_phantoms.sh" -exec bash {} \;
+find . -type f -name "generate_configs.sh" -exec bash {} \;
+find . -type f -name "simulate.sh" -exec bash {} \;
